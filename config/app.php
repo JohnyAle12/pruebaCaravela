bd0fa87800f267ffdbccbd6890af50adaf13f732
Johny Prieto <johnyprieto001@gmail.com>
2020-11-25 10:43:29 -0500

    configuración deploy

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://whispering-lowlands-80960.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
