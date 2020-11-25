f345cb4eb3ebb39a55dbfb1acaf172c055cf1ba3
Johny Prieto <johnyprieto001@gmail.com>
2020-11-25 10:39:12 -0500

    configuración deploy

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'bdtestdesarrollador.postgres.database.azure.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'pruebaCaravela'),
+            'username' => env('DB_USERNAME', 'adminbddesarrollocr@bdtestdesarrollador'),
+            'password' => env('DB_PASSWORD', 'MM9ZHY6idYJ0i'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
